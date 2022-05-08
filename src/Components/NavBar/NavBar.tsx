44f10158c25105c35e0fb5a09548871fd8910d52
Chathu Abeywickrama <chathu.abey93@gmail.com>
2022-05-08 11:42:39 +0530

    Work In Progress

---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// styles
+import './NavBar.scss';
+
+function NavBar() {
+
+  return (
+    <div className="container">
+      <h1 className='h1'>COVID STATS</h1>
+    </div>
+  );
+}
+
+export default NavBar;
--- src/Views/LandingPage/LandingPage.tsx
+++ src/Views/LandingPage/LandingPage.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import NavBar from '../../Components/NavBar/NavBar';
+
+// styles
 import './LandingPage.scss';
 
 function LandingPage() {
+
+    const [loading, setLoading] = useState(false);
+
+    const [data, setData] = useState<any>();
+
+    const getAll = () => {
+        setLoading(true);
+        axios
+          .get("https://api.covid19api.com/summary")
+          .then(function (response) {
+            console.log(response.data);
+            setData(response.data);
+            setLoading(false);
+          });
+      };
+
+    useEffect(()=>{getAll();},[])
+
   return (
-    <div className="App">
-      <p>
-         COVID STATSkk
-        </p>
+    <div>
+        <NavBar/>
+        <div className="container">
+        <p>GLOBAL STATS</p>
+        {!loading &&
+            data &&
+                <div>
+                    <p>Total Cases: {data.Global.TotalConfirmed}</p>
+                    <p>Total Deaths: {data.Global.TotalDeaths}</p>
+                </div>
+        }
+        </div>
     </div>
   );
 }
